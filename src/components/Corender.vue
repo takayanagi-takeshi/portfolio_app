edb8900ce95a3380a137373d7f93064ceefeadf3
6D157 <6d157@kanrishanoiMac.local>
2021-11-26 10:39:30 +0900

    test

--- src/components/Corender.vue
+++ src/components/Corender.vue
@@ -11,7 +11,7 @@
   <div class="month">
     <button type="button" @click="monthMinus" class="border">⇦</button>
     <div class="now_year">{{now_year}}年</div>
-    <div class="now_month">{{now_month}}月</div>
+    <div class="now_month">{{now_month}}月aaaaaaa</div>
     <button type="button" @click="monthPlus">⇨</button>
   </div>
 <!-- --------------- -->
