6b76e0a3de5109d99d43b353ab390866ac987ae9
takayanagi-takesi <75218115+takayanagi-takesi@users.noreply.github.com>
2021-12-28 00:14:00 +0900

    add

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Vuex from 'vuex'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import Vuex from 'vuex';
+import router from './router';
+import store from './store';
 
-import '@/assets/css/tailwind.css'
+import '@/assets/css/tailwind.css';
 
-Vue.use(Vuex)
-Vue.config.productionTip = false
+Vue.use(Vuex);
+Vue.config.productionTip = false;
 
 // store.commit('increment')
 
@@ -20,22 +20,22 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCvMBUCIflSACAIwZxXnPcRChFvzNV_EQc",
-  authDomain: "shiftapp-98541.firebaseapp.com",
-  projectId: "shiftapp-98541",
-  storageBucket: "shiftapp-98541.appspot.com",
-  messagingSenderId: "962110940479",
-  appId: "1:962110940479:web:d05ed7d61ba22ed0689f8a"
+  apiKey: 'AIzaSyCvMBUCIflSACAIwZxXnPcRChFvzNV_EQc',
+  authDomain: 'shiftapp-98541.firebaseapp.com',
+  projectId: 'shiftapp-98541',
+  storageBucket: 'shiftapp-98541.appspot.com',
+  messagingSenderId: '962110940479',
+  appId: '1:962110940479:web:d05ed7d61ba22ed0689f8a',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // const auth = new getAuth();
-// -----------------        firebase   -----------------------------------------
+// -----------------         firebase   -----------------------------------------
 
 new Vue({
   router,
   store,
   app,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
